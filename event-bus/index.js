c8aee366124023e7fdc15f0df97c6887b061b7ca
leijun <leijunjiang@gmail.com>
2024-06-13 16:40:42 +0200

    event sync in action 2

--- event-bus/index.js
+++ event-bus/index.js
@@ -3,21 +3,19 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-
 app.use(bodyParser.json());
+
+const events = [];
 
 app.post('/events', (req, res) => {
   const event = req.body;
-  
-  const events = [];
 
-  console.log(`event = ${event}`)
   events.push(event);
 
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
   axios.post('http://localhost:4002/events', event);
-  axios.post('http://localhost:4003/events', event); 
+  axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
 });
@@ -28,4 +26,4 @@ app.get('/events', (req, res) => {
 
 app.listen(4005, () => {
   console.log('Listening on 4005');
-})
+});
--- posts/index.js
+++ posts/index.js
@@ -8,26 +8,28 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {}
+const posts = {};
 
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
+
   posts[id] = {
     id,
     title
   };
-  console.log(`posts = ${posts}`)
-  axios.post('http://localhost:4005/events', {
+
+  await axios.post('http://localhost:4005/events', {
     type: 'PostCreated',
     data: {
-      id, title
+      id,
+      title
     }
-  })
+  });
 
   res.status(201).send(posts[id]);
 });
@@ -36,9 +38,8 @@ app.post('/events', (req, res) => {
   console.log('Received Event', req.body.type);
 
   res.send({});
-})
-
+});
 
 app.listen(4000, () => {
-   console.log('Listening on 4000')
+  console.log('Listening on 4000');
 });
